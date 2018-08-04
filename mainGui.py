075d560eeac23c4f192f49297d5df64369fac31b
Max Branvall <maxbranvall@gmail.com>
2018-08-03 19:33:25 -0500

    created mainGui.py

---
+++ mainGui.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import QtWidgets as Qw
+
+
+class Program(Qw.QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        pass

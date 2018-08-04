a90ca05eef4ad3c1f46e9cbac818aafca8a4689e
Max Branvall <maxbranvall@gmail.com>
2018-08-03 19:58:18 -0500

    setup mainGui size

--- mainGui.py
+++ mainGui.py
@@ -10,5 +10,26 @@ class Program(Qw.QMainWindow):
 
         self.initUI()
 
+        self.resize(750, 650)
+        self.setWindowTitle('Game Scraper')
+
+        self.center()
+        self.show()
+
     def initUI(self):
         pass
+
+    def center(self):
+
+        windowFrame = self.frameGeometry()
+        centerOfMonitor = Qw.QDesktopWidget().availableGeometry().center()
+
+        windowFrame.moveCenter(centerOfMonitor)
+        self.move(windowFrame.topLeft())
+
+
+if __name__ == '__main__':
+
+    app = Qw.QApplication(sys.argv)
+    mainProgram = Program()
+    sys.exit(app.exec_())

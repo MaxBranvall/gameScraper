31aeb309d9bc8653417800fc218914db66af047d
Max Branvall <maxbranvall@gmail.com>
2018-08-08 00:05:29 -0500

    main layout finished, functionality WIP

--- mainGui.py
+++ mainGui.py
@@ -3,65 +3,163 @@
 
 import sys, backend
 from PyQt5 import QtWidgets as Qw
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QFormLayout, QPushButton, QLabel, QLineEdit, QComboBox)
 from PyQt5 import (QtCore, QtGui)
 
 
 class Program(Qw.QWidget):
 
+    PLATFORMS = ['Selections:', 'Xbox One', 'PlayStation 4', 'PC', 'Nintendo Switch']
+
     def __init__(self):
 
         super().__init__()
 
-        self.resize(600, 350)
-        self.setWindowTitle('Game Scraper')
         # self.setStyleSheet("background-image: url(Images_and_HTML/bg.jpg)")
-        self.setStyleSheet("QWidget { background-color: grey }")
+        # self.setStyleSheet("QWidget { background-color: grey }")
         # self.customSheet = 'QLabel {\
             # color: orange;\
             # background-color: blue;\
         # }'
 
+
+        self.mainTitleFont = QtGui.QFont('Sans Serif', 40, 30) # Font Name, Size, Weight, Italics
+        self.subTitleFont = QtGui.QFont('Sans Serif', 30, 10)
+
+        self.resize(1, 1) # W x H
+        self.center() # centers window on screen
+        self.setWindowTitle('The Frugal Gamer')
+
         self.initUI()
-
-    def initUI(self):
-
-        titleFont = (QtGui.QFont('Meiryo', 20))
-
-        # self.welcomeTitle = Qw.QLabel('Game Scraper', self)
-        # self.welcomeTitle.setFont(titleFont)
-        # # self.welcomeTitle.setStyleSheet(self.customSheet)
-        # self.welcomeTitle.adjustSize()
-
-        self.textBox = Qw.QLineEdit(self)
-        self.textBox.resize(110, 20)
-
-        self.enter = Qw.QPushButton('Submit', self)
-        self.enter.move(20, 60)
-        self.enter.clicked.connect(self.sendToBackend)
-
-        self.clear = Qw.QPushButton('Clear', self)
-        self.clear.resize(40, 30)
-        self.clear.move(20, 40)
-        self.clear.clicked.connect(self.clearTextBox)
-
-        # vbox = Qw.QVBoxLayout()
-        # vbox.addWidget(self.enter)
-        # vbox.addWidget(self.clear)
-        # self.setLayout(vbox)
-        
-        self.center()
         self.show()
 
-    def sendToBackend(self):
+        mainScreenLayout = QVBoxLayout()
+        mainScreenLayout.addLayout(self.welcomeTitleHbox)
+        mainScreenLayout.addLayout(self.topVBox)
+        mainScreenLayout.addLayout(self.chooseGameHbox)
+        mainScreenLayout.addLayout(self.gameTextboxHox)
+        mainScreenLayout.addLayout(self.choosePlatformHbox)
+        mainScreenLayout.addLayout(self.platformSelectorHbox)
+        mainScreenLayout.addLayout(self.middleVBox)
+        mainScreenLayout.addLayout(self.bottomVBox)
+        mainScreenLayout.addLayout(self.searchAndClearHbox)
 
-        gameChoice = self.textBox.text()
-        backend.inputOutputHandling.recieveUserInput(gameChoice)
-
-    def clearTextBox(self):
-        self.textBox.clear()
+        self.setLayout(mainScreenLayout)
 
 
-    def center(self):
+    def initUI(self):
+        
+        # All widgets will be created here
+
+        self.welcomeTitle = QLabel('Welcome Frugal Gamer!')
+        self.chooseGameLabel = QLabel('Type a Game: ')
+        self.choosePlatformLabel = QLabel('Choose a Platform: ')
+
+        self.searchButton = QPushButton('Search')
+        self.savedSearchesButton = QPushButton('Saved Searches')
+        self.clearTextboxButton = QPushButton('Clear')
+
+        self.chooseGameTextbox = QLineEdit()
+        self.choosePlatformBox = QComboBox()
+
+        # Modify and add widget functionality here
+
+        self.welcomeTitle.setFont(self.mainTitleFont)
+        self.chooseGameLabel.setFont(self.subTitleFont)
+        self.choosePlatformLabel.setFont(self.subTitleFont)
+        self.choosePlatformBox.addItems(self.PLATFORMS)
+
+        self.searchButton.clicked.connect(self.printResults)
+        self.clearTextboxButton.clicked.connect(self.clearTextbox)
+
+        # Call layout methods here
+        self.labelLayout()
+        self.buttonAndInputLayout()
+        self.extraVBoxes()
+
+    def printResults(self):
+
+        try:
+            gameChoice = self.chooseGameTextbox.text()
+            platFormChoice = self.choosePlatformBox.currentText()
+
+            if (platFormChoice == self.PLATFORMS[0] or gameChoice == '' or ' '):
+                raise ValueError
+        
+        except ValueError:
+            # display warning message in window
+            print('Error.. No platform selected')
+        
+        else:
+            print(gameChoice, platFormChoice)
+            self.chooseGameTextbox.clear()
+
+
+    def clearTextbox(self):
+        self.chooseGameTextbox.clear()
+
+    def labelLayout(self):
+
+        welcomeTitleForm = QFormLayout()
+        welcomeTitleForm.addRow(self.welcomeTitle)
+
+        self.welcomeTitleHbox = QHBoxLayout()
+        self.welcomeTitleHbox.addStretch()
+        self.welcomeTitleHbox.addLayout(welcomeTitleForm)
+        self.welcomeTitleHbox.addStretch()
+
+        chooseGameForm = QFormLayout()
+        chooseGameForm.addRow(self.chooseGameLabel)
+
+        self.chooseGameHbox = QHBoxLayout()
+        self.chooseGameHbox.addStretch()
+        self.chooseGameHbox.addLayout(chooseGameForm)
+        self.chooseGameHbox.addStretch()
+
+        choosePlatformForm = QFormLayout()
+        choosePlatformForm.addRow(self.choosePlatformLabel)
+
+        self.choosePlatformHbox = QHBoxLayout()
+        self.choosePlatformHbox.addStretch()
+        self.choosePlatformHbox.addLayout(choosePlatformForm)
+        self.choosePlatformHbox.addStretch()
+
+    def buttonAndInputLayout(self):
+
+        chooseGameTextboxForm = QFormLayout()
+        chooseGameTextboxForm.addRow(self.chooseGameTextbox)
+
+        self.gameTextboxHox = QHBoxLayout()
+        self.gameTextboxHox.addStretch(3)
+        self.gameTextboxHox.addLayout(chooseGameTextboxForm)
+        self.gameTextboxHox.addWidget(self.clearTextboxButton)
+        self.gameTextboxHox.addStretch(3)
+
+        choosePlatformBoxForm = QFormLayout()
+        choosePlatformBoxForm.addRow(self.choosePlatformBox)
+
+        self.platformSelectorHbox = QHBoxLayout()
+        self.platformSelectorHbox.addStretch()
+        self.platformSelectorHbox.addLayout(choosePlatformBoxForm)
+        self.platformSelectorHbox.addStretch()
+
+        self.searchAndClearHbox = QHBoxLayout()
+        self.searchAndClearHbox.addWidget(self.savedSearchesButton)
+        self.searchAndClearHbox.addStretch()
+        self.searchAndClearHbox.addWidget(self.searchButton)
+
+    def extraVBoxes(self):
+
+        self.topVBox = QVBoxLayout()
+        self.topVBox.addStretch()
+
+        self.middleVBox = QVBoxLayout()
+        self.middleVBox.addStretch()
+
+        self.bottomVBox = QVBoxLayout()
+        self.bottomVBox.addStretch()
+
+    def center(self): # Centers window on screen
 
         windowFrame = self.frameGeometry()
         centerOfMonitor = Qw.QDesktopWidget().availableGeometry().center()
@@ -69,11 +167,17 @@ class Program(Qw.QWidget):
         windowFrame.moveCenter(centerOfMonitor)
         self.move(windowFrame.topLeft())
 
+class GUI_IO:
+    
+    def sendToBackend(self):
 
+        gameChoice = self.textBox.text()
+        backend.inputOutputHandling.recieveUserInput(gameChoice)
 
-
-if __name__ == '__main__':
-
+def main():
     app = Qw.QApplication(sys.argv)
     mainProgram = Program()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
